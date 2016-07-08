905d0911dd0ca29540f2922385a71784c944771b
Mayur Pipaliya <mayuraho@macbook-air.localdomain>
2016-07-09 00:00:32 +0530

    12:00 changes

--- test/test.js
+++ test/test.js
@@ -16,26 +16,35 @@ if (typeof jQuery != 'undefined') {
 			var _user = [];
 			var rawUserData = html.split('tbody class="align-top"');
 
-			i=0;
-			/*
-			console.log(rawUserData.length);
-			console.log ( rawUserData[49].split('no-color hover-link weight-600">')[1].split('</div>')[0]);
-			*/
+			i=1;
 			
-			while(i<rawUserData.length-3){
-			var _name 		= rawUserData[i].split('no-color hover-link weight-600">')[1].split('</div>')[0];
-			var _handle 	= rawUserData[i].split('gray-text body-font-small hover-link">')[1].split('</div>')[0];
-			// var _score  	= rawUserData[i].split('dark ajax-modal weight-600 block')[1].split('</a>')[0].split(">")[1];
-			// var _score_url 	= rawUserData[i].split('dark ajax-modal weight-600 block')[1].split('</a>')[0].split(">")[0].split('"')[4];
+			console.log(rawUserData.length);
+			// console.log ( rawUserData[49].split('no-color hover-link weight-600">')[1].split('</div>')[0]);
+			
+			
+			while(i<rawUserData.length){
+				var _name 		= rawUserData[i].split('no-color hover-link weight-600">')[1].split('</div>')[0];
+				var _handle 	= rawUserData[i].split('gray-text body-font-small hover-link">')[1].split('</div>')[0];
+				var _score  	= rawUserData[i].split('dark ajax-modal weight-600 block')[1].split('</a>')[0].split(">")[1];
+				var _score_url 	= rawUserData[i].split('dark ajax-modal weight-600 block')[1].split('</a>')[0].split(">")[0].split('"')[4];
 
-			_user['name'] 	= _name;
-			_user['handle'] = _handle;
-			// _user['score'] 	= _score;
-			// _user['score_url'] 	= "https://www.hackerearth.com" +_score_url;
-			i++;
+				if(_score>100){
+				item = {}
+				item["name"] 		= _name;
+				item["handle"]		= _handle;
+				item["score"] 		= _score;
+				item["score_url"]	= _score_url;
+
+
+				_user.push(item);
+				//_user['handle'] = _handle;
+				// _user['score'] 	= _score;
+				// _user['score_url'] 	= "https://www.hackerearth.com" +_score_url;
+				}
+				i++;
 			}
 			
-			console.log( _user);
+			console.log(_user);
 	  }
 	  });
 	}
